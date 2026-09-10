05a206bb524f08b86186576d5998b003eff1cdb1
Zaeemasifmaxobix <zaeem.asif@maxobiz.com>
2026-09-10 16:56:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello from Node.js"
+    });
+});
+
+app.listen(4000, () => {
+    console.log("Server running on port 4000");
+});
